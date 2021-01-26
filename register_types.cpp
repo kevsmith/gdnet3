2a127e0e381de1807d08271259f3f3486d58464e
Kevin Smith <lambada.calculus@gmail.com>
2021-01-25 19:46:32 -0500

    Fix compile errors against 3.2.x

--- register_types.cpp
+++ register_types.cpp
@@ -1,27 +1,23 @@
 /* register_types.cpp */
 
-#include "core/error_macros.h"
-#include "core/class_db.h"
 #include "register_types.h"
 
-#include "penet/penet.h"
-
+#include "core/class_db.h"
+#include "core/error_macros.h"
 #include "gdnet_address.h"
 #include "gdnet_event.h"
 #include "gdnet_message.h"
 #include "gdnet_peer.h"
+#include "penet/penet.h"
 
 void register_gdnet3_types() {
-	ClassDB::register_virtual_class<GDNetPeer>();
-	ClassDB::register_virtual_class<GDNetEvent>();
-	ClassDB::register_virtual_class<GDNetMessage>();
-	ClassDB::register_class<GDNetHost>();
-	ClassDB::register_class<GDNetAddress>();
+  ClassDB::register_virtual_class<GDNetPeer>();
+  ClassDB::register_virtual_class<GDNetEvent>();
+  ClassDB::register_virtual_class<GDNetMessage>();
+  ClassDB::register_class<GDNetHost>();
+  ClassDB::register_class<GDNetAddress>();
 
-	if (penet_initialize() != 0)
-		ERR_EXPLAIN("Unable to initialize PENet");
+  if (penet_initialize() != 0) print_error("Unable to initialize PENet");
 }
 
-void unregister_gdnet3_types() {
-	penet_deinitialize();
-}
+void unregister_gdnet3_types() { penet_deinitialize(); }
